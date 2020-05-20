28eb072ca09a9b3096442a5a8b3e69f9485fd139
A-by-alimelon <alimelon97@gmail.com>
2020-05-21 01:06:21 +0900

    done

---
+++ Assets/ItemBox.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    private Renderer myRenderer;
+    public Color touchColor;
+    private Color originalColor;
+    public bool isOveraped = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //트리거인 콜라이더와 충돌했을 때 자동 실행
+    //Enter 충돌을 한 그 순간
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "EndPoint"){
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+            
+        }
+        
+    }
+
+    //Eixt 붙어있다가 떼어질 때
+    void OnTriggerExit(Collider other) {
+        if(other.tag == "EndPoint"){
+            isOveraped = false;
+            myRenderer.material.color = originalColor;
+            
+        }
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(other.tag == "EndPoint"){
+            isOveraped = true;
+            myRenderer.material.color = touchColor;
+            
+        } 
+    }
+
+    //stay 충돌하고 있는 동안
+
+    //일반 콜라이더와 충돌했을 때 자동 실행
+    // void OnCollisionEnter(Collision other){
+
+    // }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody playerRigidbody;
+    public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // 화면이 한 번 깜빡일 때, 한 번 실행
+    // 1초에 대략 60번 단, 사양에 따라 다르다
+    // 몇 번 실행되는 지는 정해져 있지 않다. 
+    void Update()
+    {
+        if(gameManager.isGameOver==true){
+            return;
+        }
+        //유저 입력을 넣자
+        float inputX = Input.GetAxis("Horizontal");
+        float inputZ = Input.GetAxis("Vertical");
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity = velocity * speed;
+        velocity.y = fallSpeed;
+
+        playerRigidbody.velocity = velocity;
+    }
+}
